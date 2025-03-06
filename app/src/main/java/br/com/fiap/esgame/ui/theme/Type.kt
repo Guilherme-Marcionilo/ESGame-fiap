fdf0587e45420c155c09af682ed49c4dc544cd47
Gabriela May Canarin <gabi_may@hotmail.com>
2025-03-06 19:42:08 -0300

    Adiciona Fonte Poppins

--- app/src/main/java/br/com/fiap/esgame/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/esgame/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package br.com.fiap.esgame.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.esgame.R
+
+//Font Poppins
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
