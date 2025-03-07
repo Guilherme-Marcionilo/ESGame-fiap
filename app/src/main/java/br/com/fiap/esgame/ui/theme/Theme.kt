c7289400a72b836c50bc6a8419462fbaef023be8
Gabriela May Canarin <gabi_may@hotmail.com>
2025-03-06 23:55:24 -0300

    Adiciona cores ao projeto

--- app/src/main/java/br/com/fiap/esgame/ui/theme/Theme.kt
+++ app/src/main/java/br/com/fiap/esgame/ui/theme/Theme.kt
@@ -10,10 +10,17 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+
+val BluePrimary = Color(0xFF002D62)
+
+val GreenHighlight = Color(0xFF00B140)
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
